9e28900c28548985079f8f5b9b3b75681cea6d3d
RaHughes <rogerallenhughes@hotmail.com>
2021-10-14 14:26:31 -0500

    Add functionality for create_fleet method

--- fleet.py
+++ fleet.py
@@ -1,6 +1,14 @@
+from battleship import Battleship
+from destroyer import Destroyer
+from submarine import Submarine
+from aircraft_carrier import AircraftCarrier
+
 class Fleet:
     def __init__(self):
         self.ships = []
 
-    def create_fleet():
-        pass    
+    def create_fleet(self):
+        self.ships.append(Submarine())
+        self.ships.append(Destroyer())
+        self.ships.append(Battleship())
+        self.ships.append(AircraftCarrier()) 
